16043598229a3ab4b8ea8d4332d8329b9fe72a06
CodeOmari <jacksonomari2230@gmail.com>
2025-02-18 14:59:09 +0300

    Implementing User Authentication in Django

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
 from .views import list_books
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('books/', views.book_list_view, name='book_list'),  # URL for the function-based view
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  # URL for the class-based view
+
+    path('register/', views.register, name='register'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,6 +3,9 @@ from .models import Book
 from .models import Library
 from django.views.generic import DetailView
 from django.views.generic.detail import DetailView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth.views import LoginView, LogoutView
 
 # Create your views here.
 
@@ -21,3 +24,23 @@ class LibraryDetailView(DetailView):
        context = super().get_context_data(**kwargs)
        context['books'] = self.object.books.all()
        return context
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user) # automatically logins the user after registration
+            return redirect('book_list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form':form})
+
+# Login view using Django's built-in LoginView
+class CustomLoginView(LoginView):
+    template_name = 'relationship_app/login.html'
+
+# Logout view using Django's built-in LogoutView
+class CustomLogoutView(LogoutView):
+    template_name = 'relationship_app/logout.html'
