8a9cd9cf3138ecb72159a21c9ccd7da274027791
CodeOmari <jacksonomari2230@gmail.com>
2025-02-18 15:38:50 +0300

    Implementing Role-Based Access Control in Django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -11,43 +11,6 @@ from .models import UserProfile
 
 # Create your views here.
 
-def book_list_view(request):
-    books = Book.objects.all()
-    context = {'books': books}
-    return render(request, 'relationship_app/list_books.html', context)
-
-
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
-
-    def get_context_data(self, **kwargs):
-       context = super().get_context_data(**kwargs)
-       context['books'] = self.object.books.all()
-       return context
-
-
-def register(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user) # automatically logins the user after registration
-            return redirect('book_list')
-    else:
-        form = UserCreationForm()
-    return render(request, 'relationship_app/register.html', {'form':form})
-
-# Login view using Django's built-in LoginView
-class CustomLoginView(LoginView):
-    template_name = 'relationship_app/login.html'
-
-# Logout view using Django's built-in LogoutView
-class CustomLogoutView(LogoutView):
-    template_name = 'relationship_app/logout.html'
-
-
 # Check if the user is an admin
 def is_admin(user):
     return user.is_authenticated and user.userprofile.role == 'Admin'
@@ -89,3 +52,46 @@ def is_member(user):
 @user_passes_test(is_member)
 def member_view(request):
     return render(request, 'relationship_app/member_view.html')
+
+
+
+
+
+
+
+
+def book_list_view(request):
+    books = Book.objects.all()
+    context = {'books': books}
+    return render(request, 'relationship_app/list_books.html', context)
+
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+
+    def get_context_data(self, **kwargs):
+       context = super().get_context_data(**kwargs)
+       context['books'] = self.object.books.all()
+       return context
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user) # automatically logins the user after registration
+            return redirect('book_list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form':form})
+
+# Login view using Django's built-in LoginView
+class CustomLoginView(LoginView):
+    template_name = 'relationship_app/login.html'
+
+# Logout view using Django's built-in LogoutView
+class CustomLogoutView(LogoutView):
+    template_name = 'relationship_app/logout.html'
