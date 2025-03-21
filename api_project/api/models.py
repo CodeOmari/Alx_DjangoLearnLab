a24eca366457a081f8cf6af2b2c9864de5d037b8
CodeOmari <jacksonomari2230@gmail.com>
2025-03-21 10:19:55 +0300

    Setting Up a New Django Project with Django REST Framework

---
+++ api_project/api/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return f'{self.title} - {self.author}'
+
+    
+    class Meta:
+        db_table = 'Book'
