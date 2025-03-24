dc026f6a4b5091a39cec8184fb842ee3beb77d67
CodeOmari <jacksonomari2230@gmail.com>
2025-03-24 11:28:38 +0300

    Implementing Posts and Comments Functionality

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -22,3 +22,6 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+    
+        
---
+++ social_media_api/posts/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+from .models import Post, Comment
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = settings.AUTH_USER_MODEL  # Use the custom user model
+        fields = ['id', 'username']
+
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = CustomUserSerializer(read_only=True)
+    
+    class Meta:
+        model = Post
+        fields = '__all__'
+        read_only_fields = ['created_at', 'updated_at', 'id']
+
+    def create(self, validated_data):
+        user = self.context['request'].user
+        return Post.objects.create(author=user, **validated_data)
+
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = '__all__'
+        read_only_fields = ['id', 'created_at', 'updated_at']
+    
+    def create(self, validated_data):
+        user = self.context['request'].user
+        return Comment.objects.create(author=user, **validated_data)
